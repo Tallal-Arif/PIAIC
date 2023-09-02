6ffcf02cee4e427ef8442a0ba6608de5cff42d87
Tallal-Arif <143870894+Tallal-Arif@users.noreply.github.com>
2023-09-02 19:46:27 +0500

    Add files via upload

---
+++ TYPESCRIPT/WEEK-1/index.js
@@ -0,0 +1,116 @@
+//QUestion 1
+// var arr = [1,2,3,4,5,6,7,8,9,10]
+// var index = 5
+// function insertion(array:number[], ind:number)
+// {
+//     array.splice(ind, 0, 69)
+//     return array
+// }
+// console.log(insertion(arr, index))
+//Question 2
+// var cart = ["item1", "item2", "item3", "item4", "item5", "item6"]
+// var ind = 5
+// function addition(cart:string[], index:number)
+// {
+//     cart.splice(index, 0, "item69")
+// }
+// addition(cart, ind)
+// console.log("After addition: " + cart)
+// function updation(cart:string[], index:number)
+// {
+//     cart.splice(index, 1)
+//     cart.splice(index, 0 , "item88")
+// }
+// updation(cart, ind)
+// console.log("After updation: " + cart)
+// function subtraction(cart:string[], index:number)
+// {
+//     cart.splice(index, 1)
+// }
+// subtraction(cart, ind)
+// console.log("After subtraction: " + cart)
+//Question 3
+// var count = 0;
+// function display()
+// {
+//     while (count < 25)
+//     {
+//         console.log(count);
+//         count++;
+//     }
+// }
+// display();
+//Question 4
+// var count = 0;
+// var num = 0;
+// function display()
+// {
+//     while (count < 10)
+//     {
+//         if(num % 2 == 0)
+//         {
+//             console.log(num);
+//             count++;
+//         }
+//         num++;
+//     }
+// }
+// display();
+//Question 5
+// var num = 10
+// function fact(num)
+// {
+//     var factorial = 1
+//     while(num > 1)
+//     {
+//         factorial = num * factorial
+//         num--
+//     }
+//     return factorial
+// }
+// console.log("Factorial = " + fact(num))
+//Question 6
+// var array = [1,2,3,4,-6,-7,-45,5,6,7,8,9,10]
+// function remover(array:number[])
+// {
+//     var l
+//     l = array.length;
+//     for(var i = 0; i < l; ++i)
+//     {
+//         if(array[i] < 0)
+//         {
+//             array.splice(i,1)
+//             i--
+//         }
+//     }
+// }
+// remover(array);
+// console.log(array);
+//Question 7
+// var array = [1,2,3,4,5,6,7,8,9,10]
+// function addition(array:number[])
+// {
+//     var sum = 0
+//     var l = array.length;
+//     for(var i = 0; i < l; ++i)
+//     {
+//         sum += array[i];
+//     }
+//     return sum;
+// }
+// console.log("SUM = " + addition(array))
+//Question 8
+var array = [1.0, 10.0, 12.5, 18.7, 90.0];
+function temperature(array) {
+    var l = array.length;
+    var i = 0;
+    while (i < l) {
+        var F = 0;
+        F = ((9 / 5) * array[i]) + 32;
+        array.splice(i, 1);
+        array.splice(i, 0, F);
+        ++i;
+    }
+}
+temperature(array);
+console.log("COnverted array = " + array);
---
+++ TYPESCRIPT/WEEK-2/index.js
@@ -0,0 +1,84 @@
+//Question 1
+// var count = 20
+// function additon(count:number)
+// {
+//     var sum = 0
+//     var num = 0;
+//     for(var i = 0; i < count;)
+//     {
+//         if(num % 2 == 0)
+//         {
+//             sum += num;
+//             ++i
+//         }
+//         ++num;
+//     }
+//     return sum;
+// }
+// console.log("SUM = " + additon(count))
+//Question 2
+// var array = [1,2,3,4,5,6,7,8,9,10]
+// function display(array:number[])
+// {
+//     var l = array.length
+//     for(var i = 0; i < l; ++i)
+//     {
+//         if(array[i] % 2 == 0)
+//         {
+//             console.log(array[i])
+//         }
+//     }
+// }
+// display(array)
+//Question 3
+//var array = [1,2,3,4,5,6,7,8,9,10]
+// function remover(array:number[])
+// {
+//     var l = array.length
+//     for(var i = 0; i < l; ++i)
+//     {
+//         if(array[i] % 2 == 0)
+//         {
+//             array.splice(i,1)
+//             --i;
+//         }
+//     }
+// }
+// remover(array)
+// console.log(array)
+//Question 4
+// var rad = 23.4
+// function area(rad:number)
+// {
+//     return 3.14 * rad * rad
+// }
+// console.log(area(rad))
+//Question 5
+// var array = [50,23,45,60,70,64,31,90,81]
+// function remover(array:number[])
+// {
+//     var l = array.length
+//     for(var i = 0; i < l; ++i)
+//     {
+//         if(array[i] < 50)
+//         {
+//             array.splice(i,1)
+//             --i;
+//         }
+//     }
+// }
+// remover(array)
+// console.log(array)
+//Question 6
+var array = [50, 23, 45, 60, 70, 64, 31, 90, 81];
+function largest(array) {
+    var max = array[0];
+    var l = array.length;
+    for (var i = 0; i < l; ++i) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+    return max;
+}
+console.log("MAX = " + largest(array));
